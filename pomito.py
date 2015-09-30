9bc84b2e1332b6fec8b4e5ef69cc77316ebf7ad0
Arun <pratikarun@gmail.com>
2015-09-30 22:13:21 +0530

    Initial bootstrap code for esky with local directory.

--- pomito.py
+++ pomito.py
@@ -3,7 +3,24 @@
 # Main executable
 
 if __name__ == "__main__":
+    import os
     import sys
     from pomito import main
+
+    if os.getenv("enable_esky") is not None:
+        import traceback
+        import esky
+        from esky.finder import LocalVersionFinder
+
+        # Experimental esky bootstrap
+        if getattr(sys, "frozen", False):
+            version_finder = LocalVersionFinder("/tmp/pomito_repo")
+            app = esky.Esky(sys.executable, version_finder)
+
+        try:
+            app.auto_update()
+        except Exception as e:
+            print("ERROR UPDATING APP:", e)
+            print(traceback.format_exc())
 
     sys.exit(main.main())
