56a5cbf2644ed9d95e4b0459eefda89fa37f8117
Arun Mahapatra <pratikarun@gmail.com>
2018-02-06 07:40:12 +0530

    fix: add module name to logger.

--- pomito/__init__.py
+++ pomito/__init__.py
@@ -1,3 +1,4 @@
+"""Pomito package initialization."""
 # Init module
 # TODO:
 # - Set configuration parameters
@@ -8,6 +9,6 @@ import logging
 
 logger = logging.getLogger('pomito')
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(asctime)-6s: %(threadName)s: [%(levelname)s] %(message)s'))
+handler.setFormatter(logging.Formatter('%(asctime)-6s: %(threadName)s: %(name)s: [%(levelname)s] %(message)s'))
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
