0c4d8078308beec2b6a022cf57fc054544bdfd70
Arun Mahapatra <pratikarun@gmail.com>
2018-02-26 07:28:05 +0530

    chore: don't instantiate task widget in ui test.

--- tests/plugins/ui/test_task_widget.py
+++ tests/plugins/ui/test_task_widget.py
@@ -2,13 +2,13 @@
 """Tests for task widget."""
 
 import os
-# import pytest
+import pytest
 
-from pomito.plugins.ui.qt.task_widget import TaskWindow
+# from pomito.plugins.ui.qt.task_widget import TaskWindow
 from pomito.test import PomitoTestFactory
 
 
-# @pytest.fixture(scope="function")
+@pytest.fixture(scope="function")
 def task_window(qtbot):
     factory = PomitoTestFactory()
     use_trello = False
@@ -24,18 +24,19 @@ def task_window(qtbot):
             }
             factory.config_data["plugins"]["task"] = "trello"
 
-    pomodoro_service = factory.create_fake_service()
-    task_window = TaskWindow(pomodoro_service)
-    qtbot.addWidget(task_window)
-    return task_window
+    # pomodoro_service = factory.create_fake_service()
+    # task_window = TaskWindow(pomodoro_service)
+    # qtbot.addWidget(task_window)
+    # return task_window
+    return None
 
 
-# @pytest.mark.integration
-# def test_task_widget_lists_tasks(qtbot, task_window):
+@pytest.mark.integration
+def test_task_widget_lists_tasks(qtbot, task_window):
     # task_window.initialize()
 
     # with qtbot.waitSignal(task_window.task_selected):
     #    task_window.get_task()
 
     # assert task_window.list_task is not None
-    # pass
+    pass
